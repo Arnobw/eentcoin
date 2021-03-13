b85a28d1565b06756572a9e98fe6ef15e4f411eb
Arnobw <arnobw@hotmail.com>
2021-03-13 15:01:32 +0100

    mooi zeg

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 let counter = 0;
+let waarde = Math.floor(Math.random()* 11);
 
-$('.btn').click(function(){
+$('.mijn').click(function(){
     counter++;
     alert( "Je hebt " + counter + " eentcoin gemijnerd" );
     $('.total').html("totaal aantal munten: " + counter);
+    $('.portfolio').html("Portfolio: €" + counter * waarde);
 });
 
+$('.waarde').html("1 eentcoin = € " + waarde);
 
+
+$('.doneer').click(function(){
+    counter = 0;
+    alert("Je hebt alles weggegeven, nu is er wereldvrede!");
+    $('.total').html("totaal aantal munten: " + counter);
+    $('.portfolio').html("Portfolio: €" + counter * waarde);
+});
 
 
